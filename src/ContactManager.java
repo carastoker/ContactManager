ec84a81409bfd5d12d9df0119b9c076e2c5d36e7
Bin Ury <r.ury@tuta.io>
2017-05-25 16:27:16 -0500

    Restructured the dudes

--- src/ContactManager.java
+++ src/ContactManager.java
@@ -1,16 +1,47 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.io.*;
+import java.nio.*;
 
 /**
  * Created by cstoker on 5/25/17.
  */
 public class ContactManager {
 
+  private ArrayList<Contact> dudesList;
 
-  public static void main(String[] args) {
-
-   Contact cara = new Contact("Cara", "000");
-   System.out.println(cara.getName());
-
+  public void getContacts() {
+    for (Contact dude : this.dudesList) {
+      System.out.println(dude);
+    }
   }
 
+  public void add(Contact dude) {
+    this.dudesList.add(dude);
+  }
+
+  public ContactManager() {
+    this.dudesList = new ArrayList<Contact>();
+  }
+
+  public void writeDudes() {
+    String directory = "contacts";
+    String filename = "contacts.txt";
+    Path dudes = Paths.get(directory, filename);
+    List<String> stringDudes = new ArrayList<>();
+
+    for (Contact dude : this.dudesList) {
+     stringDudes.add(dude.toString());
+    }
+    try {
+      Files.write(dudes, stringDudes);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
   public static void main(String[] args) {
-    
+    ContactManager dudeman = new ContactManager();
+    Contact rusty = new Contact("Rusty", "000");
+    dudeman.add(rusty);
+    dudeman.writeDudes();
   }
 }
